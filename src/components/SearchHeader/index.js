4c92c8361c7eb6261cae229b0e18b568de147d1a
xiaoyuej <xiaoyuej@welldeliver.cn>
2021-12-02 17:25:54 +0800

    封装搜索栏

--- src/components/SearchHeader/index.js
+++ src/components/SearchHeader/index.js
@@ -1,37 +1,39 @@
 import React from 'react'
 import { Flex } from 'antd-mobile'
 import './index.scss'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 import PropTypes from 'prop-types'
- function SearchHeader({history,cityName}) {
-    return(
-        //   {/* 搜索框 */}
-          <Flex className="search-box">
-          {/* 左侧白色区域 */}
-          <Flex className="search">
-              {/* 位置 */}
-              <div className="location" onClick={()=> this.props.history.push('/citylist')}>
-                  <span className="name">{cityName}</span>
-                  <i className="iconfont icon-arrow"></i>
 
-              </div>
-              {/* 搜索表单 */}
-              <div className="form" onClick={()=> this.props.history.push('/search')}>
-                  <i className="iconfont icon-seach"></i>
-                  <span className="text">请输入小区或地址</span>
-              </div>
-          </Flex>
-          {/* 右侧地图图标 */}
-          <i className="iconfont icon-map" onClick={()=> this.props.history.push('/map')}></i>
-      </Flex>
+function SearchHeader({ history, cityName, className}) {
+    return (
+        //   {/* 搜索框 */}
+        <Flex className={["search-box",className || ''].join(' ')}>
+            {/* 左侧白色区域 */}
+            <Flex className="search">
+                {/* 位置 */}
+                <div className="location" onClick={() => history.push('/citylist')}>
+                    <span className="name">{cityName}</span>
+                    <i className="iconfont icon-arrow"></i>
+
+                </div>
+                {/* 搜索表单 */}
+                <div className="form" onClick={() => history.push('/search')}>
+                    <i className="iconfont icon-seach"></i>
+                    <span className="text">请输入小区或地址</span>
+                </div>
+            </Flex>
+            {/* 右侧地图图标 */}
+            <i className="iconfont icon-map" onClick={() => history.push('/map')}></i>
+        </Flex>
 
     )
 }
 
 // 添加属性校验
 SearchHeader.propTypes = {
-    cityName:PropTypes.string.isRequired
+    cityName: PropTypes.string.isRequired,
+    className: PropTypes.string
 }
 
 export default withRouter(SearchHeader)
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
+import { Flex } from 'antd-mobile'
+
+
+
 // 导入搜索栏组件
 import SearchHeader from '../../components/SearchHeader'
 
-// 获取当前定位城市信息
-const {label} = JSON.parse(localStorage.getItem('hkzf_city'))
+// 导入样式
+import styles from './index.module.css'
 
-export default class HouseList extends React.Component{
+// 获取当前定位城市信息
+const { label } = JSON.parse(localStorage.getItem('hkzf_city'))
+
+export default class HouseList extends React.Component {
     render() {
         return (
             <div>
-                <SearchHeader cityName={label} />
+                <Flex className={styles.header}>
+                    <i className="iconfont icon-back" 
+                    onClick={()=>this.props.history.go(-1)}/>
+                    <SearchHeader cityName={label} className={styles.searchHeader}/>
+                </Flex>
+
             </div>
         )
     }
